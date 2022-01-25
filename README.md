360d27e9fdcacef3c1b0cb8d3e1dab7358d3dbb5
FlaAlves <fla.alves.lima@gmail.com>
2022-01-24 22:39:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-nb2bch
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-nb2bch)
---
+++ index.ts
@@ -0,0 +1,14 @@
+let aula : string;
+aula = "conhecimento";
+let licoes : string = "exercicios";
+//console.log((aula == licoes)&& (aula != licoes));
+let tempo : number = 1;
+let meses : number = 12;
+//console.log(tempo >= meses)
+
+if (tempo < meses && aula == licoes ||meses > tempo ) {
+  console.log("homemaranhaeomelhorfilme")
+}
+
+
+
